3d2131e376d577579f5ca2761cac92bf7b193eff
Руслан <Stariy123@mail.ru>
2023-07-06 23:22:58 +0300

    Initial commit

---
+++ Hamster.java
@@ -0,0 +1,5 @@
+public class Hamster {
+    public void makeSound(){
+        System.out.println("Haaaamster!");
+    }
+}
---
+++ Main.java
@@ -0,0 +1,7 @@
+import org.w3c.dom.ls.LSOutput;
+
+public class Main {
+ Dog dog = new Dog("gg");
+
+
+}
